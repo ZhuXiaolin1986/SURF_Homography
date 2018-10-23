4719c63f086872b6da54e10c5fadf51211fb657b
ZhuXiaolin1986 <362152625@qq.com>
2018-10-23 13:44:46 +0800

    Add munual mode

--- SURF_Homography.cpp
+++ SURF_Homography.cpp
@@ -8,16 +8,6 @@
 #include <stdio.h>
 #include <time.h>
 
-#ifndef HAVE_OPENCV_NONFREE
-
-int main(int, char**)
-{
-    printf("The sample requires nonfree module that is not available in your OpenCV distribution.\n");
-    return -1;
-}
-
-#else
-
 # include "opencv2/core/core.hpp"
 # include "opencv2/features2d/features2d.hpp"
 # include "opencv2/highgui/highgui.hpp"
@@ -30,6 +20,8 @@ int main(int, char**)
 #include <opencv2/core/core.hpp>
 
 using namespace cv;
+
+//////////////////////////////////////////////////////////////////////
 
 #define ZOOM_IN_SCALE   (4)
 #define PICS_SHOW_SCALE (0.25)
@@ -46,13 +38,26 @@ void show(const string &name, const Mat &image) {
     }
 }
 
-void readme();
-void onMouse( int event, int x, int y, int flags, void* param);
-void findObjectInScene(Mat object, Mat scene);
-void getOutputImageName(char* buf);
+void ReadMe()
+{ printf("Usage: \n"
+        "\tManu => SURF_Homography -m [-d] --scene=sceneImg.jpg\n"
+        "\tAuto => SURF_Homography [-d] --scene=sceneImg.jpg --template=objectImg.jpg \n");
+}
 
-#define OUTPUT_IMAGE_NAME_LEN_MAX      (64)
-char OutputImageName[OUTPUT_IMAGE_NAME_LEN_MAX];
+
+void onMouseForAutomatically( int event, int x, int y, int flags, void* param);
+void onMouseForManually( int event, int x, int y, int flags, void* param);
+void findObjectInScene(Mat object, Mat scene);
+void getTimeStamp(char* buf);
+
+String g_FileSaved;
+
+bool g_DebugEnable = false;
+bool g_OpManuMode = false;
+string g_SceneFileName;
+string g_ObjectFileName;
+
+//////////////////////////////////////////////////////////////////////
 
 int g_LeftTop_x = 0;
 int g_LeftTop_y = 0;
@@ -68,26 +73,58 @@ Mat scene_splitted;
 
 int minHessian = 400;
 
-/**
- * @function main
- * @brief Main function
- */
+//////////////////////////////////////////////////////////////////////
+
+#define QT_FONT_NORMAL 50
+
+String windowTitle = "Perspective Transformation Demo";
+String labels[4] = { "TL","TR","BR","BL" };
+vector< Point2f> roi_corners;
+vector< Point2f> dst_corners(4);
+int roiIndex = 0;
+bool dragging;
+int selected_corner_index = 0;
+bool validation_needed = true;
+
+//////////////////////////////////////////////////////////////////////
+
+const char* keys =
+{
+    "{m |manually|false|manually or using a template }"
+    "{d |debug|false|generate the splitted image or not }"
+    "{s |scene|sceneImg.jpg|image to process }"
+    "{t |template|template.jpg|the object you wish to find in scene }"
+};
+
+
 int main( int argc, char** argv )
 {
-  if( argc != 3 )
-  { readme(); return -1; }
+  CommandLineParser parser(argc, argv, keys);
+  g_OpManuMode = parser.get<bool>("manually");
+  g_DebugEnable = parser.get<bool>("debug");
+  g_SceneFileName = parser.get<string>("scene");
+  g_ObjectFileName = parser.get<string>("template");
+  printf("=================\n");
+  parser.printParams();
+  printf("=================\n");
 
-  img_object = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-  img_scene = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+  img_scene = imread( g_SceneFileName, CV_LOAD_IMAGE_GRAYSCALE );
+  img_object = imread( g_ObjectFileName, CV_LOAD_IMAGE_GRAYSCALE );
 
-  if( !img_object.data || !img_scene.data )
-  { printf(" --(!) Error reading images \n"); return -1; }
+  if(g_OpManuMode)
+    goto MAUALLY_PROCESS;
+
+AUTOMATICALLY_PROCESS:
+  if( img_object.empty() || img_scene.empty() )
+  {
+    ReadMe();
+    exit(-1);
+  }
 
   img_scene_copy = img_scene.clone();
   namedWindow("scene");
   show("scene", img_scene_copy);
-  setMouseCallback("scene", onMouse, 0);
-  printf("=================\n");
+  setMouseCallback("scene", onMouseForAutomatically, 0);
 
   for(;;)
   {
@@ -105,7 +142,7 @@ int main( int argc, char** argv )
       destroyAllWindows();
       namedWindow("scene");
       show("scene", img_scene);
-      setMouseCallback("scene", onMouse, 0);
+      setMouseCallback("scene", onMouseForAutomatically, 0);
       printf("=================\n");
     }
 
@@ -126,17 +163,132 @@ int main( int argc, char** argv )
     }
 
   }
+  printf("=================\n");
+  return 0;
 
+MAUALLY_PROCESS:
+  if( img_scene.empty() )
+  {
+    ReadMe();
+    exit(-1);
+  }
+
+  Mat original_image = img_scene.clone();
+  Mat image, warped_image;
+
+  roi_corners.push_back(Point2f( (float)(original_image.cols/4), (float)(original_image.rows/4) ));
+  roi_corners.push_back(Point2f( (float)(original_image.cols/4*3), (float)(original_image.rows/4*1) ));
+  roi_corners.push_back(Point2f( (float)(original_image.cols/4*3), (float)(original_image.rows/4*3) ));
+  roi_corners.push_back(Point2f( (float)(original_image.cols/4), (float)(original_image.rows/4*3) ));
+
+  namedWindow(windowTitle, WINDOW_NORMAL);
+  namedWindow("Warped Image", WINDOW_AUTOSIZE);
+  moveWindow("Warped Image", 80, 20);
+  resizeWindow(windowTitle, 960, 720);
+  moveWindow(windowTitle, 800, 10);
+
+  setMouseCallback(windowTitle, onMouseForManually, 0);
+
+  bool endProgram = false;
+  while (!endProgram)
+  {
+    if ( validation_needed & (roi_corners.size() < 4) )
+    {
+      validation_needed = false;
+      image = original_image.clone();
+
+      for (size_t i = 0; i < roi_corners.size(); ++i)
+      {
+        circle( image, roi_corners[i], 17, Scalar(0, 255, 0), 7 );
+
+        if( i > 0 )
+        {
+          line(image, roi_corners[i-1], roi_corners[(i)], Scalar(0, 0, 255), 2);
+          circle(image, roi_corners[i], 17, Scalar(0, 255, 0), 7);
+          putText(image, labels[i].c_str(), roi_corners[i], QT_FONT_NORMAL, 2, Scalar(255, 0, 0), 5);
+        }
+      }
+      imshow( windowTitle, image );
+    }
+
+    if ( validation_needed & ( roi_corners.size() == 4 ))
+    {
+      validation_needed = false;
+      image = original_image.clone();
+      for ( int i = 0; i < 4; ++i )
+      {
+          line(image, roi_corners[i], roi_corners[(i + 1) % 4], Scalar(0, 0, 255), 2);
+          circle(image, roi_corners[i], 17, Scalar(0, 255, 0), 7);
+          putText(image, labels[i].c_str(), roi_corners[i], QT_FONT_NORMAL, 3, Scalar(255, 0, 0), 5);
+      }
+      imshow( windowTitle, image );
+
+      dst_corners[0].x = 0;
+      dst_corners[0].y = 0;
+      dst_corners[1].x = (float)std::max(norm(roi_corners[0] - roi_corners[1]), norm(roi_corners[2] - roi_corners[3]));
+      dst_corners[1].y = 0;
+      dst_corners[2].x = (float)std::max(norm(roi_corners[0] - roi_corners[1]), norm(roi_corners[2] - roi_corners[3]));
+      dst_corners[2].y = (float)std::max(norm(roi_corners[1] - roi_corners[2]), norm(roi_corners[3] - roi_corners[0]));
+      dst_corners[3].x = 0;
+      dst_corners[3].y = (float)std::max(norm(roi_corners[1] - roi_corners[2]), norm(roi_corners[3] - roi_corners[0]));
+
+      Size warped_image_size = Size(cvRound(dst_corners[2].x), cvRound(dst_corners[2].y));
+      Mat H = findHomography(roi_corners, dst_corners); //get homography
+      warpPerspective(original_image, warped_image, H, warped_image_size); // do perspective transformation
+      show("Warped Image", warped_image);
+    }
+
+    char c = (char)waitKey( 10 );
+
+    if ((c == 'q') | (c == 'Q') | (c == 27))
+    {
+      endProgram = true;
+      char ts[32] = {0};
+      getTimeStamp(ts);
+      printf("\n%s ==> \n[%d, %d, %d, %d, %d, %d, %d, %d]\n", ts,
+        (int)round((roi_corners[0]).x), (int)round((roi_corners[0]).y),
+        (int)round((roi_corners[1]).x), (int)round((roi_corners[1]).y),
+        (int)round((roi_corners[2]).x), (int)round((roi_corners[2]).y),
+        (int)round((roi_corners[3]).x), (int)round((roi_corners[3]).y));
+
+      if(!g_DebugEnable) continue;
+
+      g_FileSaved = g_SceneFileName;
+      size_t n = g_FileSaved.find_last_of(".", -1);
+      g_FileSaved.insert(n, ts);
+      imwrite(g_FileSaved, warped_image);
+    }
+
+    if ((c == 'h') | (c == 'H'))
+    {
+      roi_corners[selected_corner_index].x = roi_corners[selected_corner_index].x - 1;
+      validation_needed = true;
+    }
+
+    if ((c == 'j') | (c == 'J'))
+    {
+      roi_corners[selected_corner_index].y = roi_corners[selected_corner_index].y + 1;
+      validation_needed = true;
+    }
+
+    if ((c == 'k') | (c == 'K'))
+    {
+      roi_corners[selected_corner_index].y = roi_corners[selected_corner_index].y - 1;
+      validation_needed = true;
+    }
+
+    if ((c == 'l') | (c == 'L'))
+    {
+      roi_corners[selected_corner_index].x = roi_corners[selected_corner_index].x + 1;
+      validation_needed = true;
+    }
+  }
+
+  printf("=================\n");
   return 0;
 }
 
-/**
- * @function readme
- */
-void readme()
-{ printf(" Usage: ./SURF_Homography <img1> <img2>\n"); }
-
-void onMouse( int event, int x, int y, int flags, void* param)
+void onMouseForAutomatically( int event, int x, int y, int flags, void* param)
 {
     if(g_InProgress != 0) return;
 
@@ -249,7 +401,9 @@ void findObjectInScene(Mat img_object, Mat img_scene)
   line( img_matches, scene_corners[2] + offset, scene_corners[3] + offset, Scalar( 0, 255, 0), LINE_WIDTH );
   line( img_matches, scene_corners[3] + offset, scene_corners[0] + offset, Scalar( 0, 255, 0), LINE_WIDTH );
 
-  printf("[%d, %d, %d, %d, %d, %d, %d, %d]\n", 
+  char ts[32] = {0};
+  getTimeStamp(ts);
+  printf("\n%s ==> \n[%d, %d, %d, %d, %d, %d, %d, %d]\n", ts,
     (int)round((scene_corners[0]).x) + g_LeftTop_x, (int)round((scene_corners[0]).y) + g_LeftTop_y,
     (int)round((scene_corners[1]).x) + g_LeftTop_x, (int)round((scene_corners[1]).y) + g_LeftTop_y,
     (int)round((scene_corners[2]).x) + g_LeftTop_x, (int)round((scene_corners[2]).y) + g_LeftTop_y,
@@ -263,24 +417,61 @@ void findObjectInScene(Mat img_object, Mat img_scene)
   H = findHomography(scene_corners, obj_corners);
   warpPerspective(img_scene, warped_image, H, warped_image_size);
 
-
-  getOutputImageName(OutputImageName);
-  
-  imwrite(OutputImageName, warped_image);
-  show(OutputImageName, warped_image);
+  if(g_DebugEnable)
+  {
+    g_FileSaved = g_SceneFileName;
+    size_t n = g_FileSaved.find_last_of(".", -1);
+    g_FileSaved.insert(n, ts);
+    imwrite(g_FileSaved, warped_image);
+  }
+  show("Warped Image", warped_image);
 
   g_InProgress = 0;
 }
 
-void getOutputImageName(char* buf)
+void getTimeStamp(char* buf)
 {
     time_t now;
     struct tm* tm_now;
 
-    memset(buf, 0, OUTPUT_IMAGE_NAME_LEN_MAX);
     time(&now);
     tm_now = localtime(&now);
-    snprintf(buf, OUTPUT_IMAGE_NAME_LEN_MAX, "SPLITTED_%d_%02d_%02d_%02d_%02d_%02d.jpg",  tm_now->tm_year+1900, tm_now->tm_mon, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
+    snprintf(buf, 32, "@%d_%02d_%02d_%02d_%02d_%02d",  tm_now->tm_year+1900, tm_now->tm_mon, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
 }
 
-#endif
+//////////////////////////////////////////////////////////////////////
+
+void onMouseForManually( int event, int x, int y, int flags, void* param)
+{
+    // Action when left button is pressed
+    if (roi_corners.size() == 4)
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            if ((event == EVENT_LBUTTONDOWN) & ((abs(roi_corners[i].x - x) < 20)) & (abs(roi_corners[i].y - y) < 20))
+            {
+                selected_corner_index = i;
+                dragging = true;
+            }
+        }
+    }
+    else if ( event == EVENT_LBUTTONDOWN )
+    {
+        roi_corners.push_back( Point2f( (float) x, (float) y ) );
+        validation_needed = true;
+    }
+
+    // Action when left button is released
+    if (event == EVENT_LBUTTONUP)
+    {
+        dragging = false;
+    }
+
+    // Action when left button is pressed and mouse has moved over the window
+    if ((event == EVENT_MOUSEMOVE) && dragging)
+    {
+        roi_corners[selected_corner_index].x = (float) x;
+        roi_corners[selected_corner_index].y = (float) y;
+        validation_needed = true;
+    }
+}
